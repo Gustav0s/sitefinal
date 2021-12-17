90404dcff628ef1b47318ce090674d39bc9256b8
Gustav0s <90979904+Gustav0s@users.noreply.github.com>
2021-12-17 18:29:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function saudacao(){
+    let nome= prompt("Digite seu nome");
+    let msg= `Olá ${nome}, seja muito bem-vindo(a) ao Psicolors, um site sobre a Psicologia das Cores, espero que se divirta`;
+    alert(msg);
+}
+saudacao();
